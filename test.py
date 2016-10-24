44a05a3d66d1cbb0767c2cc9c9de9589065547fc
Michael Wisslead <michael.wisslead@gmail.com>
2016-10-24 00:37:46 -0500

    Simplified the code a bit.

--- test.py
+++ test.py
@@ -1,20 +1,51 @@
-import json
-
 from tree_walk_with_alternates import TreeWalker
 
-with open('test.json', 'rb') as fid:
-    data = json.loads(fid.read().decode('UTF-8'))
+levels = ['level1', 'level2']
+levels_plural = ['firsts', 'seconds']
+data = {
+    "level1": {
+        "option1": {
+            "value1": "level1 = option1"
+        },
+        "option2": {
+            "level2": {
+                "option2": {
+                    "value1": "level1 = option2, level2 = option2"
+                }
+            },
+            "value2": "level1 = option2"
+        }
+    },
+    "level2": {
+        "option1": {
+            "value1": "level2 = option1"
+        }
+    },
+    "value1": "top level",
+    "value2": "top level"
+}
 
-tree = TreeWalker(data, ['level1', 'level2'], ['firsts', 'seconds'])
+tree = TreeWalker(data, levels, levels_plural)
+
+try:
+   tree['Wrong'] = 3
+except Exception as err:
+    import traceback
+    traceback.print_exc(err)
+    pass
 
 print(tree['value1'])
 print(tree['value2'])
+print(tree['firsts'])
 tree['level1'] = 'option1'
+print(tree['seconds'])
 print(tree['value1'])
 print(tree['value2'])
 tree['level1'] = 'option2'
+print(tree['seconds'])
 print(tree['value1'])
 print(tree['value2'])
 tree['level2'] = 'option2'
 print(tree['value1'])
 print(tree['value2'])
+print(tree['value3'])
--- tree_walk_with_alternates/treewalk.py
+++ tree_walk_with_alternates/treewalk.py
@@ -1,18 +1,10 @@
-from __future__ import print_function
-
-import sys
-import json
-
-def _valid_descents(items):
-    if len(items) < 1:
-        return []
-    if len(items) == 1:
-        return [items]
-    retval = []
-    for i in range(1,len(items)):
-        for x in _valid_descents(items[i:]):
-            retval.append([items[0]] + x)
-    return retval
+def _find_key(data, key, levels):
+    for i, (level, setting) in enumerate(levels):
+        try:
+            return _find_key(data[level][setting], key, levels[i+1:])
+        except:
+            pass
+    return data[key]
 
 class TreeWalker(object):
     def __init__(self, input_dict, levels, levels_plural):
@@ -20,31 +12,22 @@ class TreeWalker(object):
         self._levels = levels
         self._levels_plural = levels_plural
         self._settings = {}
-
-    def _find_item(self, key, max_level=-1):
-        levels = self._levels
-        if max_level>=0:
-            levels = self._levels[:max_level]
-        for pattern in _valid_descents([None] + levels + [None]):
-            data = self._data
-            try:
-                for level in pattern[1:-1]:
-                    data = data[level]
-                    data = data[self[level]]
-                return data[key]
-            except KeyError:
-                pass
-        return self._data[key]
+        for level in levels:
+            self._settings[level] = None
 
     def __getitem__(self, key):
         if key in self._levels:
             return self._settings[key]
         if key  in self._levels_plural:
             ind = self._levels_plural.index(key)
-            return list(self._find_item(self._levels[ind], ind).keys())
-        return self._find_item(key)
+            key = self._levels[ind]
+            levels = [(level, self._settings[level]) for level in self._levels[:ind]]
+            return list(_find_key(self._data, key, levels))
+        return _find_key(self._data, key, [(level, self._settings[level]) for level in self._levels])
 
     def __setitem__(self, key, val):
+        if key not in self._levels:
+            raise KeyError(key)
         self._settings[key] = val
 
     def keys(self):
